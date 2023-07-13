cbc44124cbf14a4bc018d84c538b76292bba59fd
SarasAnalytics-Daton <69815945+saras-daton@users.noreply.github.com>
2023-07-13 08:04:57 +0530

    Update ExchangeRates.sql

--- models/Currency Exchange Rates/ExchangeRates.sql
+++ models/Currency Exchange Rates/ExchangeRates.sql
@@ -20,7 +20,7 @@ SELECT coalesce(MAX(_daton_batch_runtime) - 2592000000,0) FROM {{ this }}
 {% endif %}
 
 {% set table_name_query %}
-    {{set_table_name('%exchange_rates')}}    
+    {{set_table_name('%exchangerates')}}    
 {% endset %} 
 
 {% set results = run_query(table_name_query) %}
